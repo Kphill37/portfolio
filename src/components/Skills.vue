00408b89dd4d5aedc7a3e02a824b84869c6c9b23
Kphill37 <fooxiefloof@gmail.com>
2019-07-31 10:13:13 -0600

    commit

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-    <div class="row Skills">
+    <div class="row LandF">
       <div class="col-2">
         <h1 id="LandF">
           Languages
@@ -34,19 +34,16 @@
         </h1>
       </div>
       <div class="col-2">
-        <img src="../assets/HTML5-250.png" alt />
+        <img src="../assets/MongoDB-250.png" alt />
       </div>
       <div class="col-2">
-        <img src="../assets/JS-250.png" alt />
+        <img src="../assets/mysql-250.png" alt />
       </div>
       <div class="col-2">
-        <img src="../assets/CSS3-250.png" alt />
+        <img src="../assets/Node-250.png" alt />
       </div>
       <div class="col-2">
-        <img src="../assets/C-250.png" alt />
-      </div>
-      <div class="col-2">
-        <img src="../assets/vuejs.png" alt />
+        <img id="dotnet" src="../assets/1200px-.NET_Core_Logo.svg.png" alt />
       </div>
     </div>
   </div>
@@ -60,9 +57,13 @@
 
 <style>
 .Skills {
-  margin-top: 40vh;
+  margin-top: 50vh;
 }
 #LandF {
   justify-content: center;
+}
+#dotnet {
+  height: 220px;
+  width: 220px;
 }
 </style>
